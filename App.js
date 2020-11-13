8b6f17aacf048b0abcf0a730904ab32761875b4f
Catherine Waind <catherine.waind@bbc.co.uk>
2020-11-13 10:51:28 +0000

    added Date tp page and /photo route

--- App.js
+++ App.js
@@ -3,6 +3,12 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("frontend"))
+
+app.get('/photo',(req,res) => {
+ res.send("Photo of the day");
+
+}); 
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
