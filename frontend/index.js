ce78bd915e524db83b07ec9286063461c06066a1
Catherine Waind <catherine.waind@bbc.co.uk>
2020-11-17 09:35:33 +0000

    Additional of Sol Time to page

--- frontend/index.js
+++ frontend/index.js
@@ -22,3 +22,24 @@ function kennedyCentreOffsetTime() {
   document.getElementById("timeOffset").appendChild(insertingTheOffsetTime);
 }
 kennedyCentreOffsetTime();
+
+function photoOfTheDay() {
+  var NasaPhotoRef = "https://www.nasa.gov/";
+  const photoTag = document.getElementById("photoOfTheDay");
+  var photoOfTheDay = NasaPhotoRef;
+  photoTag.appendChild(photoOfTheDay);
+}
+
+function displayTheCuriositySol() {
+  const landedDate = new Date(2011, 10, 16, 15, 02);
+
+  const todaysDate = new Date();
+
+  const differenceMs = todaysDate - landedDate;
+  const dateDifference = differenceMs / (1000 * 60 * 60 * 24);
+  const sols = dateDifference / 1.0274912517;
+
+  const curiositySol = document.getElementById("currentSol");
+  curiositySol.innerHTML = sols;
+}
+displayTheCuriositySol();
