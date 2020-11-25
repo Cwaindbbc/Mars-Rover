69fb075e1993902a594cd907d6d095260cb19e49
Catherine Waind <catherine.waind@bbc.co.uk>
2020-11-25 10:44:32 +0000

    added rover page and image

--- App.js
+++ App.js
@@ -3,21 +3,13 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("frontend"));
-app.use(express.static("frontend/login.html"));
+app.use("/login", express.static("frontend/login.html"));
+app.use("/rover", express.static("frontend/rover.html"));
 
 app.get("/photo", (req, res) => {
   res.send("Photo");
 });
 
-app.get("/rover", (req, res) => {
-  res.send(" Rover page");
-});
-
-app.get("/login", (req, res) => {
-  res.send("Login page");
-});
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-  0;
 });
