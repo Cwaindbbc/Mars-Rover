63b87df395c6429fdef8cc4bffb145a2be56bdc7
Catherine Waind <catherine.waind@bbc.co.uk>
2020-12-11 14:46:59 +0000

    refactor the photo fetch

--- fetchingFromNasaApi.js
+++ fetchingFromNasaApi.js
@@ -1,14 +1,17 @@
 const fetch = require("node-fetch");
 const configParams = require("./config.js");
 
-function fetchNasa() {
+function fetchNasa(req,res,next) {
   const url = "https://api.nasa.gov/planetary/apod/";
   const key = configParams.apikey;
   const fullURL = `${url}?api_key=${key}`;
 
   return fetch(fullURL)
     .then((nasaRes) => nasaRes.json())
-    .then((data) => res.json(data));
+    .then((data) => res.json(data))
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 module.exports = fetchNasa;
